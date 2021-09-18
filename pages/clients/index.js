3e3264de2edf64008506b2476d2c101fec672bf8
ahsanarique <ahsan.arique@gmail.com>
2021-09-18 17:39:44 +0600

    dynamic nested links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    {
+      id: 1,
+      name: "client 1",
+    },
+    {
+      id: 2,
+      name: "client 2",
+    },
+    {
+      id: 3,
+      name: "client 3",
+    },
+  ];
+
   return (
     <div>
       <h1>Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };

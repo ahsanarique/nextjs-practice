0e5e41c7462d94ae76598f02144293efdd53a1d2
ahsanarique <ahsan.arique@gmail.com>
2021-09-19 15:55:10 +0600

    dynamic fallback + 404 error

--- pages/[pid].js
+++ pages/[pid].js
@@ -18,15 +18,25 @@ const ProductDetailPage = (props) => {
   );
 };
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "fakeData", "fakeData.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -36,12 +46,12 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
+    paths: pathsWithParams,
     fallback: true, // or a string instead of boolean. Slower approach but loads fully finished page.
   };
 }
